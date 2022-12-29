cc2df4f1fe6b9e809d504063d7ec9915121c63e2
Luis Palma <palmaluis1997@gmail.com>
2022-12-29 18:20:19 -0400

    change

--- app/Http/Controllers/PaymentReportController.php
+++ app/Http/Controllers/PaymentReportController.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 class PaymentReportController extends Controller
 {
-    public function getPaymentTotals()
+    public function getPaymentTotals(Request $request)
     {
-        $payments = InvoicePayment::selectRaw('payment_methods.name, SUM(invoice_payments.amount) as paymentTotal, coins.name as coin_name, coins.symbol as coin_symbol')
+        $payments = InvoicePayment::selectRaw('payment_methods.id,payment_methods.name,SUM(invoice_payments.amount) as paymentTotal, coins.name as coin_name, coins.symbol as coin_symbol')
             ->join('payment_methods', 'payment_methods.id', 'invoice_payments.payment_method_id')
             ->join('coins', 'coins.id', 'invoice_payments.coin_id')
             ->groupBy('payment_method_id', 'coin_id', 'payment_methods.name', 'coins.name', 'coins.symbol')
