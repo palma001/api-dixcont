cc19a94d4448d8978c0b793131f206ad21783821
Luis Palma <palmaluis1997@gmail.com>
2022-12-29 18:28:41 -0400

    change

--- app/Http/Controllers/PaymentReportController.php
+++ app/Http/Controllers/PaymentReportController.php
@@ -12,6 +12,8 @@ class PaymentReportController extends Controller
         $payments = InvoicePayment::selectRaw('payment_methods.id,payment_methods.name,SUM(invoice_payments.amount) as paymentTotal, coins.name as coin_name, coins.symbol as coin_symbol')
             ->join('payment_methods', 'payment_methods.id', 'invoice_payments.payment_method_id')
             ->join('coins', 'coins.id', 'invoice_payments.coin_id')
+            ->whereDate('invoice_payments.created_at', '>=', $request->from)
+			->whereDate('invoice_payments.created_at', '<=', $request->to)
             ->groupBy('payment_method_id', 'coin_id', 'payment_methods.name', 'coins.name', 'coins.symbol')
             ->get();
 
